46ecddddbab14ebe41a4723c3d6ef2fc5d7f8e0c
mac stephens <macstephens@gmail.com>
2023-06-28 16:38:40 -0400

    docker compose working (we pray)

--- data/pintrip.sql
+++ data/pintrip.sql
@@ -1,35 +1,34 @@
-
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS buckets;
+DROP TABLE IF EXISTS drops;
+DROP TABLE IF EXISTS bucket_drops; 
 
 CREATE TABLE users (
     id SERIAL NOT NULL UNIQUE,
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    username TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL UNIQUE
     -- password??????
-    
-
 );
 
 CREATE TABLE buckets (
     id SERIAL NOT NULL UNIQUE,
     title TEXT NOT NULL,
     username TEXT NOT NULL UNIQUE,
-    cover_photo BLOB,
+    cover_photo BYTEA,
     description TEXT NOT NULL,
     url TEXT NOT NULL, 
-    user_id INTEGER NOT NULL REFERENCES users('id') ON DELETE CASCADE
-     
+    user_id INTEGER NOT NULL REFERENCES users("id") ON DELETE CASCADE
 );
 
 CREATE TABLE drops (
     id SERIAL NOT NULL UNIQUE,
     name TEXT NOT NULL,
-    photo BLOB,
+    photo BYTEA,
     description TEXT NOT NULL,
     city TEXT NOT NULL,
     address TEXT NOT NULL,
     url TEXT NOT NULL
-    
 );
 
 CREATE TABLE bucket_drops (
