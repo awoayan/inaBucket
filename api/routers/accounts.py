d2f78eb59ee9299a14884a337c0226f63e38c0cc
Brian <brian.j.ashcraft@gmail.com>
2023-06-30 15:08:57 -0400

    revert here

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -21,7 +21,7 @@ from queries.accounts import (
 
 
 class AccountForm(BaseModel):
-    email: str
+    username: str
     password: str
 
 
@@ -71,6 +71,6 @@ async def create_account(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Cannot create an account with those credentials",
         )
-    form = AccountForm(email=info.email, password=info.password)
+    form = AccountForm(username=info.email, password=info.password)
     token = await authenticator.login(response, request, form, repo)
     return AccountToken(account=account, **token.dict())
