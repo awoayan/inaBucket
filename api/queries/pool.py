b86a96d2c023b93f1197ebe47cd78267c532b7de
Amanda <asouthern24@gmail.com>
2023-06-30 16:41:07 -0400

    working on apis

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -1,30 +1,96 @@
 import os
 from psycopg_pool import ConnectionPool
 
+
 pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
 
 class BucketQueries:
+    def get_buckets(self):
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT a.id, a.full_name,
+                        a.email, a.username,
+                        b.id, b.title, b.username,
+                        b.cover_photo, b.description, b.url,
+                        b.user_id
+
+                    FROM accounts a 
+                    JOIN buckets b ON(a.id = b.user_id)
+                
+                    GROUP BY 
+                        a.id, a.full_name, a.username, b.email,
+                        b.id, b.title, b.username, b.cover_photo, 
+                        b.description,
+                        b.url, b.user_id
+
+                    ORDER BY b.title
+                    """,
+                )
+
+                buckets = []
+                rows = cur.fetchall()
+                for row in rows:
+                    bucket = self.bucket_record_to_dict(row, cur.description)
+                    buckets.append(bucket)
+                return buckets
+            
+    def get_bucket(self, bucket_id):
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT a.id, a.full_name,
+                        a.email, a.username,
+                        b.id, b.title, b.username,
+                        b.cover_photo, b.description, b.url,
+                        b.user_id
+                    FROM accounts a
+                    JOIN buckets b ON(a.id = b.user_id)
+                    WHERE b.id = %s
+                    """,
+                    [bucket_id],
+                )
+
+                row = cur.fetchone()
+                if row is None:
+                    return None
+                return self.bucket_record_to_dict(row)
+
     def create_bucket(self, bucket):
-            id=None
+            id = None
             with pool.connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(
                         """
                         INSERT INTO buckets
-                            ( title, cover_photo, description, url )
+                            ( title, username, cover_photo, description, url, user_id )
                         VALUES 
-                            ( %s, %s, %s, %s)
+                            ( %s, %s, %s, %s, %s, %s)
                         RETURNING id;
                         """,
                         [
                             bucket.title, 
+                            bucket.username,
                             bucket.cover_photo,
                             bucket.description,
                             bucket.url,
                             bucket.user_id,
                         ]
                     )
-                    row=cur.fetchone()
+                    row = cur.fetchone()
                     id = row[0]
             if id is not None:
-                return {"message": "failed to create bucket"}
+                return self.get_bucket(id)
+
+    def bucket_record_to_dict(self, row):
+        return (
+            id=row[0],
+            title=row[1],
+            username=row[2],
+            cover_photo=row[3],
+            description=row[4],
+            url=row[5],
+            user_id=row[6]
+        )
--- api/routers/buckets.py
+++ api/routers/buckets.py
@@ -1,20 +1,19 @@
+from typing import Literal
 from fastapi import APIRouter, Depends, Response
 from pydantic import BaseModel
+
+
 from queries.pool import BucketQueries
+from queries.accounts import AccountOut
+
 router = APIRouter()
+
 
 class Error(BaseModel):
     message: str
-    
-class BucketIn(BaseModel):
-    title: str
-    cover_photo: str
-    description: str
-    url: str
-    user_id: int
 
-class BucketOut(BaseModel):
-    id: int
+
+class BucketIn(BaseModel):
     title: str
     username: str
     cover_photo: str
@@ -22,9 +21,33 @@ class BucketOut(BaseModel):
     url: str
     user_id: int
 
+
+class BucketOut(BaseModel):
+    id: int
+    title: str
+    cover_photo: str
+    description: str
+    url: str
+    username: AccountOut
+    user_id: AccountOut  
+
+
 @router.post("/api/buckets", response_model=BucketOut)
 def create_bucket(
     bucket: BucketIn,
-    queries:BucketQueries=Depends(),
+    queries: BucketQueries = Depends(),
 ):
     return queries.create_bucket(bucket)
+
+
+@router.get("/api/buckets/{bucket_id}", response_model=BucketOut)
+def get_bucket(
+    bucket_id: int,
+    response: Response,
+    queries: BucketQueries = Depends(),
+):
+    record = queries.get_bucket(bucket_id)
+    if record is None:
+        response.status_code = 404
+    else:
+        return record
