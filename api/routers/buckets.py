08b7f3aefb1b9c7aae1f2eeddaafeef638c26067
Amanda <asouthern24@gmail.com>
2023-07-10 15:21:47 -0400

    DELETE function works

--- api/routers/buckets.py
+++ api/routers/buckets.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 from fastapi import APIRouter, Depends, Response
 from pydantic import BaseModel
 
@@ -32,6 +32,12 @@ class BucketOut(BaseModel):
     ## See above - shouldnt username be accountOut? cant get it to work with just account out 
     owner: AccountOut  
 
+class BucketPatch(BaseModel):
+    title: Optional[str]
+    cover_photo: Optional[str]
+    descriptionxxx: Optional[str]
+    url: Optional[str]
+    
 
 @router.post("/api/buckets", response_model=BucketOut)
 def create_bucket(
@@ -52,3 +58,24 @@ def get_bucket(
         response.status_code = 404
     else:
         return record
+    
+
+@router.put("/api/buckets/{bucket_id}", response_model=BucketOut)
+def update_bucket(
+    bucket_id: int,
+    bucket_in: BucketIn,
+    response: Response,
+    queries: BucketQueries = Depends(),
+
+):
+    record = queries.update(bucket_id, bucket_in)
+    if record is None:
+        response.status_code = 404
+    else:
+        return record
+
+
+@router.delete("/api/buckets/{bucket_id}", response_model=bool)
+def delete_bucket(bucket_id: int, queries: BucketQueries = Depends()):
+    queries.delete_bucket(bucket_id)
+    return True
