20b3ad89c3f619c10d96c87835db2e677ecdf74f
Ayan <foxtrotsin9@gmail.com>
2023-07-16 14:57:04 -0400

    nav bar login-logout

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-// import Nav from "./Nav";
+import Nav from "./Nav";
 
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
@@ -13,6 +13,7 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
+			<Nav/>
 				<Routes>
 					<Route
 						path="/"
