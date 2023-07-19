138e2c004864f66c1fa62c90f2f27d1508d77e0a
Brian <brian.j.ashcraft@gmail.com>
2023-07-19 15:32:53 -0400

    card tweaks

--- ghi/src/ProfilePage.js
+++ ghi/src/ProfilePage.js
@@ -3,88 +3,83 @@ import { useGetTokenQuery } from './app/api';
 import Notification from './Notification';
 
 function ProfilePage() {
-    const [buckets, setBuckets] = useState([])
-    
+    const [buckets, setBuckets] = useState([]);
+
     useEffect(() => {
-        const fetchBuckets = async () => {
-            const url = 'http://localhost:8000/api/buckets'
-            const response = await fetch(url)
-            
+            const fetchBuckets = async () => {
+            const url = 'http://localhost:8000/api/buckets';
+            const response = await fetch(url);
+
             if (response.ok) {
-                const data = await response.json()
-                setBuckets(data)
-                console.log(data)
+                const data = await response.json();
+                setBuckets(data);
+                console.log(data);
             } else {
-                console.error(response)
+                console.error(response);
             }
-        }
+            };
 
-        fetchBuckets()
-    }, [])
+    fetchBuckets();
+    }, []);
 
-    const { data: tokenData} = useGetTokenQuery()
-    console.log("hello line 26")
-    
-    let userBuckets = null
-    
-     if (!tokenData) {
+    const { data: tokenData } = useGetTokenQuery();
+
+    let userBuckets = null;
+
+    if (!tokenData) {
         return (
-            <div className="container">
-                <Notification type="info"> Please Login</Notification>
-            </div>
-        )
+        <div className="container">
+            <Notification type="info"> Please Login</Notification>
+        </div>
+        );
     } else {
         userBuckets = buckets.filter(
-            (bucket) => bucket.owner.id === tokenData.account.id);
-       
-        
+        (bucket) => bucket.owner.id === tokenData.account.id
+        );
     }
-   console.log(userBuckets)
 
-   return  (
-        <div>
-        <h2>These are buckets on cards</h2>
-        <div className="columns">
+return (
+        <div style={{ background: '#e6f2f5', padding: '10px' }}>
+        <h1 class="is-size-5 has-text-weight-bold" >'s Profile</h1>
+        <div className="columns is-multiline">
             {userBuckets.map((bucket) => (
-            <div className="column" key={bucket.id}>
-                <div className="card">
+            <div
+                className="column is-one-fifth"
+                key={bucket.id}
+                style={{ transition: 'transform 0.2s' }}
+                onMouseEnter={(e) => {
+                e.currentTarget.style.transform = 'scale(1.05)';
+                }}
+                onMouseLeave={(e) => {
+                e.currentTarget.style.transform = 'scale(1)';
+                }}
+            >
+                <div className="card" style={{ width: '300px', maxHeight: '500px', border: 0 }}>
                 <div className="card-image">
-                    <figure className="image is-4by3">
-                    <img
-                        src={bucket.cover_photo}
-                        alt={bucket.title}
-                    />
+                    <figure className="image is-1by1">
+                    <img src={bucket.cover_photo} alt={bucket.title} />
                     </figure>
                 </div>
                 <div className="card-content">
                     <div className="media">
                     <div className="media-left">
                         <figure className="image is-48x48">
-                        <img
-                            src={bucket.owner.profile_picture}
-                            alt={bucket.owner.username}
-                        />
+                        <img src={bucket.owner.profile_picture} alt={bucket.owner.username} />
                         </figure>
                     </div>
                     <div className="media-content">
                         <p className="title is-4">{bucket.title}</p>
-                        <p className="subtitle is-6">
-                        @{bucket.owner.username}
-                        </p>
+                        <p className="subtitle is-6">@{bucket.owner.username}</p>
                     </div>
                     </div>
-                    <div className="content">
-                    {bucket.details}
-                    <br />
-                    {/* <a href="#">@bulmaio</a>. */}
-                    <br />
-                    {/* {bucket.tags.map((tag) => (
-                        <a key={tag} href="#">{tag}</a>
-                    ))} */}
-                    <br />
-                    <time dateTime={bucket.timestamp}>
-                        {bucket.timestamp}
-                    </time>
+                    <div className="content" style={{ maxHeight: '80px', overflow: 'hidden' }}>
+                    {bucket.details.length > 100 ? (
+                        <>
+                        {bucket.details.slice(0, 100)}...
+                        </>
+                    ) : (
+                        bucket.details
+                    )}
                     </div>
                 </div>
                 </div>
@@ -93,9 +88,6 @@ function ProfilePage() {
         </div>
         </div>
     );
-
 }
-
-
 
 export default ProfilePage;
