be11a3463b8b61e65fd0d7198b5dfc7421221dab
mac stephens <macstephens@gmail.com>
2023-07-24 17:43:38 -0400

    I still have to add the bucket update, but we have the code for that now in the drops

--- api/queries/drops.py
+++ api/queries/drops.py
@@ -1,6 +1,6 @@
 import os
 from psycopg_pool import ConnectionPool
-from routers.drops import DropUpdate
+
 
 
 pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
@@ -103,7 +103,7 @@ class DropQueries:
             print("returndrop:", return_drop)
             return return_drop
 
-    def update_drop(self, drop_id: int, drop: DropUpdate):
+    def update_drop(self, drop_id, drop):
         with pool.connection() as conn:
             with conn.cursor() as cur:
                 params = [
@@ -113,7 +113,7 @@ class DropQueries:
                     drop.city,
                     drop.address,
                     drop.url,
-                    drop_id,
+                    drop_id,  
                 ]
                 cur.execute(
                     """
@@ -125,14 +125,19 @@ class DropQueries:
                         address = %s,
                         url = %s
                     WHERE id = %s
-                    RETURNING *;
+                    RETURNING name, photo, details, city, address, url, creator_id
                     """,
                     params,
                 )
-
+                
+                record = None
                 row = cur.fetchone()
                 if row is not None:
-                    return self.drop_record_to_dict(row, cur.description)
+                    record = {}
+                    for i, column in enumerate(cur.description):
+                        record[column.name] = row[i]
+                print("I'M A REAL BOY")
+                return record
 
     def delete_drop(self, drop_id):
         with pool.connection() as conn:
@@ -168,8 +173,8 @@ class DropQueries:
             creator_id = {}
             creator_fields = [
                 "creator_id",
-                "full_name",
                 "email",
+                "full_name",
                 "username",
             ]
             for i, column in enumerate(description):
--- api/routers/drops.py
+++ api/routers/drops.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel
 
 from queries.drops import DropQueries
 from queries.accounts import AccountOut
-from routers.buckets import BucketOut
 
 router = APIRouter()
 
@@ -35,13 +34,14 @@ class DropOut(BaseModel):
     url: str
     creator_id: AccountOut
 
-class DropUpdate(BaseModel):
+class UpdateDropOut(BaseModel):
     name: str
     photo: str
     details: str
     city: str
     address: str
     url: str
+    creator_id: int
 
 
 @router.post("/api/drops", response_model=DropOut)
@@ -76,18 +76,18 @@ def get_drops(
     else:
         return records
     
-@router.put("/api/drops/{drop_id}", response_model=DropOut)
+@router.put("/api/drops/{drop_id}", response_model=UpdateDropOut)
 def update_drop(
     drop_id: int,
-    drop_update: DropUpdate,
+    drop: DropIn,
+    response: Response,
     queries: DropQueries = Depends(),
 ):
-    existing_drop = queries.get_drop(drop_id)
-    if existing_drop is None:
-        raise HTTPException(status_code=404, detail="Drop not found")
-
-    updated_drop = queries.update_drop(drop_id, drop_update)
-    return updated_drop
+    record = queries.update_drop(drop_id, drop)
+    if record is None:
+        response.status_code = 404
+    else:
+        return record
 
 @router.delete("/api/drops/{drop_id}", response_model=bool)
 def delete_drop(drop_id: int, queries: DropQueries = Depends()):
