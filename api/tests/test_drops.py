5e656efe715bd1b700fa5061c7be99455e3f2096
Amanda <asouthern24@gmail.com>
2023-07-26 11:49:36 -0400

    4 tests created and passing

--- api/tests/test_drops.py
+++ api/tests/test_drops.py
@@ -2,86 +2,173 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from queries.drops import DropQueries
 from main import app
+import authenticator
+import json 
 
 client = TestClient(app)
 
 
 class EmptyDropQueries:
     def get_drops(self):
-        return []
-
-
-class CreateDropQueries:
-    def create_drop(self, drop):
-        result = {
-            "id": 0,
-            "name": "string",
+        return [
+           {
+            "id": 11,
+            "name": "hii test",
             "photo": "string",
             "details": "string",
             "city": "string",
             "address": "string",
             "url": "string",
             "creator_id": {
-                "id": "string",
-                "email": "string",
-                "full_name": "string",
-                "username": "string"
+                "id": "1",
+                "email": "amanda@gmail.com",
+                "full_name": "amanda",
+                "username": "amanda"
+                }
+            },
+            {
+            "id": 12,
+            "name": "hii test 2 ",
+            "photo": "string",
+            "details": "string",
+            "city": "string",
+            "address": "string",
+            "url": "string",
+            "creator_id": {
+                "id": "1",
+                "email": "amanda@gmail.com",
+                "full_name": "amanda",
+                "username": "amanda"
+                }
             }
-        }
+        ]
+
+
+class FakeCreateDropQueries:
+    def create_drop(self, drop):
+        result = {
+        "id": 11,
+        "name": "hiii",
+        "photo": "string",
+        "details": "string",
+        "city": "string",
+        "address": "string",
+        "url": "string",
+        "creator_id": {
+            "id": "1",
+            "email": "string",
+            "full_name": "string",
+            "username": "string"
+            },
+    }
+            
+        
     
-        result.update(drop)
+        # result.update(drop)
         return result
     
 
 
+class DeleteDropQueries:
+    def delete_drop(self, drop_id):
+        result = {
+            "id": 11,
+            "name": "hii test",
+            "photo": "string",
+            "details": "string",
+            "city": "string",
+            "address": "string",
+            "url": "string",
+            "creator_id": {
+                "id": "1",
+                "email": "amanda@gmail.com",
+                "full_name": "amanda",
+                "username": "amanda"
+                }
+            },
+        return result 
+        
+
+
 def test_get_all_drops():
     #Arrange
+    
     app.dependency_overrides[DropQueries] = EmptyDropQueries
     #Act
     response = client.get("/api/drops")
+
+    expected = [
+           {
+            "id": 11,
+            "name": "hii test",
+            "photo": "string",
+            "details": "string",
+            "city": "string",
+            "address": "string",
+            "url": "string",
+            "creator_id": {
+                "id": "1",
+                "email": "amanda@gmail.com",
+                "full_name": "amanda",
+                "username": "amanda"
+                }
+            },
+            {
+            "id": 12,
+            "name": "hii test 2 ",
+            "photo": "string",
+            "details": "string",
+            "city": "string",
+            "address": "string",
+            "url": "string",
+            "creator_id": {
+                "id": "1",
+                "email": "amanda@gmail.com",
+                "full_name": "amanda",
+                "username": "amanda"
+                }
+            }
+        ]
     #Clean up
     app.dependency_overrides = {}
     #Assert
     assert response.status_code == 200
-    assert response.json() == []
+    assert response.json() == expected
 
 
 def test_create_drop():
-    #Arrange
-    app.dependency_overrides[DropQueries] = CreateDropQueries
-    json = {
-    "name": "string",
-    "photo": "string",
-    "details": "string",
-    "city": "string",
-    "address": "string",
-    "url": "string",
-    "creator_id": {
-        "id": "string",
-        "email": "string",
-        "full_name": "string",
-        "username": "string"
-        },
-    "bucket_id": 0
+    app.dependency_overrides[DropQueries] = FakeCreateDropQueries
+
+
+    
+    drop = {
+        "name": "hiii",
+        "photo": "string",
+        "details": "string",
+        "city": "string",
+        "address": "string",
+        "url": "string",
+        "creator_id": 1,
+        "bucket_id": 1 ,
     }
 
     expected = {
-    "id": 0,
-    "name": "string",
-    "photo": "string",
-    "details": "string",
-    "city": "string",
-    "address": "string",
-    "url": "string",
-    "creator_id": {
-        "id": "string",
-        "email": "string",
-        "full_name": "string",
-        "username": "string"
+        "id": 11,
+        "name": "hiii",
+        "photo": "string",
+        "details": "string",
+        "city": "string",
+        "address": "string",
+        "url": "string",
+        "creator_id": {
+            "id": "1",
+            "email": "string",
+            "full_name": "string",
+            "username": "string"
+            },
     }
-}
     #Act
-    response = client.post("/api/drops", json=json)
+    response = client.post("/api/drops", json.dumps(drop))
 
     #Clean up 
     app.dependency_overrides = {}
@@ -89,3 +176,15 @@ def test_create_drop():
     #Assert 
     assert response.status_code == 200
     assert response.json() == expected
+
+
+
+# def test_delete_drop():
+#     app.dependency_overrides[DropQueries] = DeleteDropQueries
+
+#     response = client.delete("/api/drops/11"), 
+
+#     app.dependency_overrides = {}
+
+#     assert response.status_code == 200
+#     assert response == True
